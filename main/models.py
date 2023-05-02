32f9f1b3731d78814cd81b09f0383aba5659821a
subham sarangi <ivan.sarangi@gmail.com>
2023-05-02 10:07:04 +0000

    approved comments

--- main/models.py
+++ main/models.py
@@ -14,6 +14,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def get_comments(self):
+        return Comment.objects.filter(post=self.pk)
 
 
 class Comment(models.Model):
