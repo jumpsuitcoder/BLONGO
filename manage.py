f8890d2c51b81296231de6ba4d6c46315f50d3c3
subham sarangi <ivan.sarangi@gmail.com>
2023-05-02 08:54:03 +0000

    prod settings

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'reiko.settings.local')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'reiko.settings.production')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
