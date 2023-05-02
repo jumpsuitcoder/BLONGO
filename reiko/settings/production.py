063bd81651bc0c3252430a136b6b29881721fd36
subham sarangi <ivan.sarangi@gmail.com>
2023-05-02 09:56:47 +0000

    whitenoise autorefresh

--- reiko/settings/production.py
+++ reiko/settings/production.py
@@ -23,5 +23,4 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+WHITENOISE_AUTOREFRESH = True
